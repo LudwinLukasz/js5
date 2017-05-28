4d56d2fa978b1c3a88043d8d0fbaddb4f49b3a73
Lukasz Ludwin <gorli@interia.pl>
2017-05-28 18:21:27 +0200

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+﻿var a = 0,
+	b = 0,
+	value;
+
+a = prompt('Enter a');
+b = prompt('Enter b');
+value =	(a * a) + (2 * a * b) - (b * b);
+console.log('Delta value for a:' + a + 'b: ' + b + ' is equal to: ' + value);
+if (value > 0) {
+	console.log("Wartość jest dodatnia")
+} else if (value < 0) {
+	console.log("Wartość jest ujemna")
+} else if (value == 0) {
+	console.log("Wartość jest równa 0")
+} else {
+	console.log("coś nie tak")
+}
